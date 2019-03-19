5795da15898e3762d02ee8e0e1ddf0891e875311
edu <edu@localhost.localdomain>
2019-03-19 12:40:30 -0400

    improved usability

--- PatternRecognition.py
+++ PatternRecognition.py
@@ -20,22 +20,22 @@ class PatternRecognition(object):
         self.status_code = 0
 
         # Analyse the data to recognize the pattern.
-        self.analyse_pattern()
+        self._analyse_pattern()
 
 
-    def analyse_pattern(self):
+    def _analyse_pattern(self):
         for d in self.data:
             for l in d:
                 # l => current value of a string row
                 # Check if the current value is a digit
-                self.predict_the_current_character(l)
+                self._predict_the_current_character(l)
 
             self.current_pattern += self.digit_pattern
             self.digit_pattern = ''
         
             # Check if the current pattern matches with most of the list of data.
             for c in self.data:
-                self.compare_current_string_with_current_pattern(c)
+                self._compare_current_string_with_current_pattern(c)
                 
                 # This will solve the bug if the dirty data comes first in the data list.
                 if self.mismatches == len(self.data) - 1:
@@ -70,15 +70,15 @@ class PatternRecognition(object):
                 break
             self.current_pattern = ''
 
-    def compare_current_string_with_current_pattern(self, c):
+    def _compare_current_string_with_current_pattern(self, c):
         if re.compile(r'^{}$'.format(self.current_pattern)).search(c):
             self.matches += 1
             self.formatted_list.append(f'{c}') 
         else:
             self.mismatches += 1
-            self.formatted_list.append(f'{c} <== this is currupted')
+            self.formatted_list.append(f'{c} <== dirty data')
 
-    def predict_the_current_character(self, l):
+    def _predict_the_current_character(self, l):
         if l.isdigit():
             self.repeated_times += 1
             self.digit_pattern = '\d{' + str(self.repeated_times) + '}'
@@ -93,24 +93,32 @@ class PatternRecognition(object):
             self.current_pattern += l
 
     def get_pattern(self):
-        print(self.timeout_rounds)
+        ''' Returns the predicted regex pattern '''
+        if self.status_code != 1:
+            return 'No pattern was generated'
         return f'r"^{self.pattern}$"'
 
     def print_formatted_list(self):
+        ''' Print a list of data with showing where the data is dirty '''
         for i, data in enumerate(self.formatted_list, 1):
             print(i, '=> ', data)
             
     def get_formatted_list(self):
+        ''' Works like print_formatted_list() but it returns a list instead '''
         return self.formatted_list
 
     def get_matches(self):
+        ''' Returns total number of matches '''
         return self.matches
     
     def get_mismatches(self):
+        ''' Returns total number of mismatches '''
         return self.mismatches
 
     def get_message(self):
+        ''' Return overall proccess message '''
         return self.message
 
     def get_status_code(self):
+        ''' Return overall proccess status code (Good for developers) '''
         return self.status_code
